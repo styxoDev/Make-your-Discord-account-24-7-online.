ce7004dcb9a43c36229cd5724cd51286922e5662
Styxo <96294167+styxoDev@users.noreply.github.com>
2022-01-06 17:38:26 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+/*
+   This will express the server on port.
+    Get the https url and then paste it in uptime robot to never get offline.
+ 
+Or if you have replit Hacker account then delete this file and remove 
+const app = require('./server.js'); from main.js
+
+*/
+const express = require('express');
+const app = express();
+
+app.listen('8000', () => {
+  
+})
